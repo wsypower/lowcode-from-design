080e272642f0b2aa591f82a9808ce0a98718f7e5
wangweiyi <wangweiyi_ty@163.com>
2023-04-12 11:35:58 +0800

    feat: 日期时间戳格式配置项修正

--- src/components/form-designer/setting-panel/property-editor/field-date/date-valueFormat-editor.vue
+++ src/components/form-designer/setting-panel/property-editor/field-date/date-valueFormat-editor.vue
@@ -2,27 +2,29 @@
   <el-form-item :label="i18nt('designer.setting.valueFormat')">
     <el-select v-model="optionModel.valueFormat" filterable allow-create>
       <el-option label="YYYY-MM-DD" value="YYYY-MM-DD"></el-option>
-      <el-option label="YYYY-MM-DD HH:mm:ss" value="YYYY-MM-DD HH:mm:ss"></el-option>
+      <el-option
+        label="YYYY-MM-DD HH:mm:ss"
+        value="YYYY-MM-DD HH:mm:ss"
+      ></el-option>
       <el-option label="YYYY-MM" value="YYYY-MM"></el-option>
       <el-option label="YYYY" value="YYYY"></el-option>
+      <el-option label="timestamp" value="x"></el-option>
     </el-select>
   </el-form-item>
 </template>
 
 <script>
-  import i18n from "@/utils/i18n"
+import i18n from '@/utils/i18n'
 
-  export default {
-    name: "date-valueFormat-editor",
-    mixins: [i18n],
-    props: {
-      designer: Object,
-      selectedWidget: Object,
-      optionModel: Object,
-    },
-  }
+export default {
+  name: 'date-valueFormat-editor',
+  mixins: [i18n],
+  props: {
+    designer: Object,
+    selectedWidget: Object,
+    optionModel: Object,
+  },
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
